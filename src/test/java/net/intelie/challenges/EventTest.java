45782af89863c87084806e8ee69f9432d1eb4b55
Deyve Vieira <deyvedvm@hotmail.com>
2020-04-23 22:09:07 -0300

    Update dependencies

--- src/test/java/net/intelie/challenges/EventTest.java
+++ src/test/java/net/intelie/challenges/EventTest.java
@@ -1,10 +1,11 @@
 package net.intelie.challenges;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventTest {
+
     @Test
     public void thisIsAWarning() throws Exception {
         Event event = new Event("some_type", 123L);
